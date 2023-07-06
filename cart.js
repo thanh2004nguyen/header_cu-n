197ebcd5ce949b8fb98a50883916ae610a06b133
thanh2004nguyen <116701025+thanh2004nguyen@users.noreply.github.com>
2023-07-06 17:35:52 +0700

    push header cuon

---
+++ cart.js
@@ -0,0 +1,13 @@
+var data =[
+    {"id":"p01","name":"lotus pink", "pic":"camera_width.jpg","price":100,"description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea at porro, minus, neque rerum incidunt cumque voluptate eum officiis nesciunt veritatis accusamus est temporibus aperiam deserunt nobis perferendis! Reiciendis, dolores?", "cat":"lotus"},
+    
+    {"id":"p02","name":"lotus white", "pic":"gautruc.jpg","price":150, "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea at porro, minus, neque rerum incidunt cumque voluptate eum officiis nesciunt veritatis accusamus est temporibus aperiam deserunt nobis perferendis! Reiciendis, dolores?","cat":"lotus"},
+
+    {"id":"p11","name":"orchid purple", "pic":"halowin.jpg","price":123,"description":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos doloribus facere aliquam amet obcaecati, eos quasi, unde quas odio ab cum fugit nesciunt harum earum quae, dolore excepturi provident? Ullam.Nihil magnam praesentium accusantium eligendi? Voluptatum impedit, quidem reprehenderit, itaque hic amet iure sapiente, velit dolor ratione quo eaque consequatur corporis cum iste voluptates nam dolore ipsam. Excepturi, obcaecati iusto!", "cat":"orchid"},
+
+    {"id":"p12","name":"orchid red", "pic":"hero.jpg","price":90,"description":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat inventore laboriosam, voluptates eaque magni animi. Qui ratione repellat, assumenda doloribus ex officiis veritatis, id quis nesciunt sit facere, porro fugiat.", "cat":"orchid"},
+
+    {"id":"p13","name":"orchid white", "pic":"hinh.jpg","price":115,"description":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit mollitia eius commodi voluptates, harum quidem id? Ipsam asperiores natus ullam laudantium nobis obcaecati nihil, exercitationem, et impedit non officiis esse.", "cat":"orchid"},
+
+    {"id":"p14","name":"orchid yellow", "pic":"image_thanhcuon.jpg","price":13,"description":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, veniam. Iure ullam beatae recusandae repellendus illo soluta, pariatur, eveniet repudiandae vero quo, accusantium mollitia consectetur ut possimus id quae animi?", "cat":"orchid"},
+]
